aeb5208ae2a1941fc8e00aa45df26471565690b2
Bijan Chokoufe Nejad <bijan.chokoufe@esailors.de>
2018-04-12 20:06:38 +0200

    Add comdirect support

--- dkb2homebank.py
+++ dkb2homebank.py
@@ -12,7 +12,16 @@ class dkb(csv.Dialect):
     lineterminator = '\r\n'
     quoting = csv.QUOTE_MINIMAL
 
+class comdirect(csv.Dialect):
+    delimiter = ';'
+    quotechar = '"'
+    doublequote = True
+    skipinitialspace = False
+    lineterminator = '\r\n'
+    quoting = csv.QUOTE_NONE
+
 csv.register_dialect("dkb", dkb)
+csv.register_dialect("comdirect", comdirect)
 
 dkbFieldNames = ["buchungstag",
                   "wertstellung",
@@ -26,6 +35,11 @@ dkbFieldNames = ["buchungstag",
                   "mandatsreferenz",
                   "kundenreferenz"]
 
+comdirectFieldNames = ["Buchungstag",
+                       "Wertstellung (Valuta)",
+                       "Vorgang",
+                       "Buchungstext",
+                       "Umsatz in EUR"]
 
 visaFieldNames =    ["abgerechnet",
                      "wertstellung",
@@ -48,14 +62,14 @@ def convertDkbCash(filename):
     with open(filename, 'r') as csvfile:
         dialect = csv.Sniffer().sniff(csvfile.read(1024))
         csvfile.seek(0)
-        reader = csv.DictReader(transactionLines(csvfile), dialect=dialect, fieldnames=dkbFieldNames)
+        reader = csv.DictReader(transactionLines(csvfile, "Betrag"), dialect=dialect, fieldnames=dkbFieldNames)
 
         with open("cashHomebank.csv", 'w') as outfile:
             writer = csv.DictWriter(outfile, dialect='dkb', fieldnames=homebankFieldNames)
             for row in reader:
                 writer.writerow(
                     {
-                    'date': convertDate(row["buchungstag"]),
+                    'date': convertDate(row["Buchungstag"]),
                     'paymode': 8,
                     'info': None,
                     'payee': row["beguenstigter"],
@@ -65,11 +79,41 @@ def convertDkbCash(filename):
                     'tags': None
                     })
 
+def convertComdirectCash(filename):
+    with open(filename, 'r') as csvfile:
+        reader = csv.DictReader(transactionLines(csvfile, "Buchungstag"),
+                                dialect='comdirect', fieldnames=comdirectFieldNames)
+
+        with open("cashHomebank.csv", 'w') as outfile:
+            writer = csv.DictWriter(outfile, dialect='comdirect',
+                                    fieldnames=homebankFieldNames)
+            for row in reader:
+                print ''
+                for key in comdirectFieldNames:
+                  print key, "=", row[key]   #   Debugging
+                new_row = {}
+                for key, value in row.iteritems():
+                  if (isinstance(value, list)):
+                    new_row.update({key: value[0].replace('"', '')})
+                  else:
+                    new_row.update({key: value.replace('"', '')})
+                writer.writerow(
+                    {
+                    'date': convertDate(new_row["Buchungstag"]),
+                    'paymode': 8,
+                    'info': None,
+                    'payee': new_row["Buchungstext"], # not really
+                    'memo': new_row["Buchungstext"], # not really
+                    'amount': new_row["Umsatz in EUR"],
+                    'category': None,
+                    'tags': None
+                    })
+
 def convertVisa(filename):
     with open(filename, 'r') as csvfile:
         dialect = csv.Sniffer().sniff(csvfile.read(1024))
         csvfile.seek(0)
-        reader = csv.DictReader(transactionLines(csvfile), dialect=dialect, fieldnames=visaFieldNames)
+        reader = csv.DictReader(transactionLines(csvfile, "Betrag"), dialect=dialect, fieldnames=visaFieldNames)
 
         with open("visaHomebank.csv", 'w') as outfile:
             writer = csv.DictWriter(outfile, dialect='dkb', fieldnames=homebankFieldNames)
@@ -86,11 +130,11 @@ def convertVisa(filename):
                     'tags': None
                     })
 
-def transactionLines(file):
+def transactionLines(file, identifier):
     lines = file.readlines()
     i = 1
     for line in lines:
-        if "Betrag" in line:
+        if identifier in line:
             return lines[i:]
         i = i + 1
 
@@ -104,15 +148,19 @@ def main():
 
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-v", "--visa", action="store_true", help="convert a DKB Visa account CSV file")
-    group.add_argument("-c", "--cash", action="store_true", help="convert a DKB Cash account CSV file")
+    group.add_argument("-d", "--dkbcash", action="store_true", help="convert a DKB Cash account CSV file")
+    group.add_argument("-c", "--comdirectcash", action="store_true", help="convert a comdirect Cash account CSV file")
 
     args = parser.parse_args()
 
     if args.visa:
         convertVisa(args.filename)
         print("DKB Visa file converted. Output file: 'visaHomebank.csv'")
-    elif args.cash:
+    elif args.dkbcash:
         convertDkbCash(args.filename)
+        print("DKB Cash file converted. Output file: 'cashHomebank.csv'")
+    elif args.comdirectcash:
+        convertComdirectCash(args.filename)
         print("DKB Cash file converted. Output file: 'cashHomebank.csv'")
     else:
         print("You must provide the type of the CSV file (--cash for DKB Cash, --visa for DKB Visa)")
